a20224a1279fcc6e78a769a52663d95409ed2ef7
ivanov.nickolay <ivanov.nickolay@gmail.com>
2016-09-13 23:41:10 +0300

    Восстановление алгоритма формирование анализов. реальзация алгоритма потокового вывода анализа по филиалам в периоде

--- AnalizPdvBundle/Command/AnalizPDVByOneBranchStream_Command.php
+++ AnalizPdvBundle/Command/AnalizPDVByOneBranchStream_Command.php
@@ -17,7 +17,7 @@ class AnalizPDVByOneBranchStream_Command extends ContainerAwareCommand
     protected function configure()
     {
         $this
-            ->setName('analiz_pdv:AnalizPDVByOneBranch')
+            ->setName('analiz_pdv:AnalizPDVByOneBranchStream')
             ->setDescription('analyze reestr by one branch');
     }
 
@@ -26,31 +26,43 @@ class AnalizPDVByOneBranchStream_Command extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $numBranch="578";
+        //$numBranch="578";
+        gc_enable();
         $dt=$this->getContainer()->get('doctrine');
         $em=$dt->getManager();
         $file="d:\\OpenServer525\\domains\\AnalizPDV\\web\\template\\AnalizPDV.xlsx";
         $data=new getDataFromReestrsByOne($em);
-        $write=new getWriteExcel($file);
-        $write->setParamFile(6,2016,$numBranch);
-        $f=$write->getNewFileName();
-        $arr=$data->getReestrInEqualErpn(6,2016,$numBranch);
-        $write->setDataFromWorksheet('In_reestr=edrpu',$arr,'A4');
-        unset($arr);
-        gc_collect_cycles();
-        $arr=$data->getReestrInNotEqualErpn(6,2016,$numBranch);
-        $write->setDataFromWorksheet('In_reestr<>edrpou',$arr,'A4');
-        unset($arr);
-        gc_collect_cycles();
-        $arr=$data->getReestrOutEqualErpn(6,2016,$numBranch);
-        $write->setDataFromWorksheet('Out_reestr=edrpu',$arr,'A4');
-        unset($arr);
-        gc_collect_cycles();
-        $arr=$data->getReestrOutNotEqualErpn(6,2016,$numBranch);
-        $write->setDataFromWorksheet('Out_reestr<>edrpou',$arr,'A4');
-        unset($arr);
-        gc_collect_cycles();
-        $write->fileWriteAndSave();
+        $arrAllBranch=$data->getAllBranchToPeriod(6,2016);
+        if(!empty($arrAllBranch))
+        {
+            foreach ($arrAllBranch[0] as $key=>$numBranch)
+            {
+                $output->writeln("Start analiz branch ".$numBranch);
+                $write = new getWriteExcel($file);
+                    $write->setParamFile (6 , 2016 , $numBranch);
+                    $f = $write->getNewFileName ();
+                        $arr = $data->getReestrInEqualErpn (6 , 2016 , $numBranch);
+                        $write->setDataFromWorksheet ('In_reestr=edrpu' , $arr , 'A4');
+                        unset($arr);
+                        gc_collect_cycles ();
+                    $arr = $data->getReestrInNotEqualErpn (6 , 2016 , $numBranch);
+                    $write->setDataFromWorksheet ('In_reestr<>edrpou' , $arr , 'A4');
+                    unset($arr);
+                    gc_collect_cycles ();
+                        $arr = $data->getReestrOutEqualErpn (6 , 2016 , $numBranch);
+                        $write->setDataFromWorksheet ('Out_reestr=edrpu' , $arr , 'A4');
+                        unset($arr);
+                        gc_collect_cycles ();
+                    $arr = $data->getReestrOutNotEqualErpn (6 , 2016 , $numBranch);
+                    $write->setDataFromWorksheet ('Out_reestr<>edrpou' , $arr , 'A4');
+                    unset($arr);
+                    gc_collect_cycles ();
+                    $write->fileWriteAndSave ();
+                unset($write);
+                gc_collect_cycles ();
+                $output->writeln("End analiz branch ".$numBranch);
+            }
+        }
 
     }
 }
