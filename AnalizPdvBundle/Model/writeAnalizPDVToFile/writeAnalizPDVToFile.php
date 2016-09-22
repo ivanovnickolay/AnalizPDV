4cae342b74f0264798cdd89a7d01cf03518fcf4b
ivanov.nickolay <ivanov.nickolay@gmail.com>
2016-09-22 22:48:23 +0300

    Первый коммит новой ветки создания класса writeAnalizPDVToFile для реализации алгоритмов формирование файлов анализа в одном месте. Класс сформирован

--- AnalizPdvBundle/Model/writeAnalizPDVToFile/writeAnalizPDVToFile.php
+++ AnalizPdvBundle/Model/writeAnalizPDVToFile/writeAnalizPDVToFile.php
@@ -7,6 +7,10 @@
  */
 
 namespace AnalizPdvBundle\Model\writeAnalizPDVToFile;
+use AnalizPdvBundle\Model\getDataFromSQL\getDataFromReestrsAll;
+use AnalizPdvBundle\Model\getDataFromSQL\getDataFromReestrsByOne;
+use AnalizPdvBundle\Utilits\createWriteFile\getWriteExcel;
+use Doctrine\ORM\EntityManager;
 
 
 /**
@@ -17,5 +21,90 @@ namespace AnalizPdvBundle\Model\writeAnalizPDVToFile;
  */
 class writeAnalizPDVToFile
 {
+	private $em;
+public function __construct (EntityManager $entityManager)
+{
+	$this->em=$entityManager;
+}
 
+	/**
+	 * формирование файла анализа сводного всему УЗ
+	 */
+	public function writeAnalizPDVByAllUZ(int $month, int $year)
+{
+	$file="d:\\OpenServer525\\domains\\AnalizPDV\\web\\template\\AnalizPDV_All.xlsx";
+	$data=new getDataFromReestrsAll($this->em);
+	$write=new getWriteExcel($file);
+	$write->setParamFile($month,$year,'ALL');
+	$f=$write->getNewFileName();
+	$arr=$data->getReestrInEqualErpn($month,$year);
+		$write->setDataFromWorksheet('In_reestr=edrpu',$arr,'A4');
+			unset($arr);
+	gc_collect_cycles();
+	$arr=$data->getReestrInNotEqualErpn($month,$year);
+		$write->setDataFromWorksheet('In_reestr<>edrpou',$arr,'A4');
+			unset($arr);
+	gc_collect_cycles();
+	$arr=$data->getReestrOutEqualErpn($month,$year);
+		$write->setDataFromWorksheet('Out_reestr=edrpu',$arr,'A4');
+			unset($arr);
+	gc_collect_cycles();
+	$arr=$data->getReestrOutNotEqualErpn($month,$year);
+		$write->setDataFromWorksheet('Out_reestr<>edrpou',$arr,'A4');
+			unset($arr);
+	gc_collect_cycles();
+	$write->fileWriteAndSave();
+	unset($data,$write);
+	gc_collect_cycles();
+}
+
+	/**
+	 *формирование файла анализа по одному конкретному филиалу
+	 */
+	public function writeAnalizPDVByOneBranch(int $month,int $year,string $numBranch)
+	{
+		$file="d:\\OpenServer525\\domains\\AnalizPDV\\web\\template\\AnalizPDV.xlsx";
+		$data=new getDataFromReestrsByOne($this->em);
+		$write=new getWriteExcel($file);
+		$write->setParamFile($month,$year,$numBranch);
+		$f=$write->getNewFileName();
+		$arr=$data->getReestrInEqualErpn($month,$year,$numBranch);
+		$write->setDataFromWorksheet('In_reestr=edrpu',$arr,'A4');
+		unset($arr);
+		gc_collect_cycles();
+		$arr=$data->getReestrInNotEqualErpn($month,$year,$numBranch);
+		$write->setDataFromWorksheet('In_reestr<>edrpou',$arr,'A4');
+		unset($arr);
+		gc_collect_cycles();
+		$arr=$data->getReestrOutEqualErpn($month,$year,$numBranch);
+		$write->setDataFromWorksheet('Out_reestr=edrpu',$arr,'A4');
+		unset($arr);
+		gc_collect_cycles();
+		$arr=$data->getReestrOutNotEqualErpn($month,$year,$numBranch);
+		$write->setDataFromWorksheet('Out_reestr<>edrpou',$arr,'A4');
+		unset($arr);
+		gc_collect_cycles();
+		$write->fileWriteAndSave();
+		unset($data,$write);
+		gc_collect_cycles();
+	}
+
+	/**
+	 * формирование файлов анализа по всем филиалам
+	 * каждый филиал в свой файл
+	 */
+	public function writeAnalizPDVByAllBranch(int $month,int $year)
+	{
+		$data=new getDataFromReestrsByOne($this->em);
+		$arrAllBranch=$data->getAllBranchToPeriod($month,$year);
+		if(!empty($arrAllBranch)) {
+			foreach ($arrAllBranch as $arrAll)
+			{
+				foreach ($arrAll as $key => $numBranch)
+				{
+					$this->writeAnalizPDVByOneBranch($month,$year,$numBranch);
+				}
+			}
+		}
+	}
 }
