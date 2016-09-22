18731265022330e9c7d60c2c96afb25baf22ffa2
ivanov.nickolay <ivanov.nickolay@gmail.com>
2016-09-22 20:55:01 +0300

    Первый коммит новой ветки создания класса writeAnalizPDVToFile для реализации алгоритмов формирование файлов анализа в одном месте

---
+++ AnalizPdvBundle/Model/writeAnalizPDVToFile/writeAnalizPDVToFile.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 22.09.2016
+ * Time: 20:52
+ */
+
+namespace AnalizPdvBundle\Model\writeAnalizPDVToFile;
+
+
+/**
+ * Задача класса реализация алгоритмов ормирования
+ * файлов анализов ПДВ из одного места
+ * Class writeAnalizPDVToFile
+ * @package AnalizPdvBundle\Model\writeAnalizPDVToFile
+ */
+class writeAnalizPDVToFile
+{
+
+}
