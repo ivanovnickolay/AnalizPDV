9fea70807b6a7183677ac87b7ff761d2fff9eac8
ivanov.nickolay <ivanov.nickolay@gmail.com>
2017-01-16 00:16:26 +0200

    Разработаны формы поиска данных в ЕРПН и РПН

--- AnalizPdvBundle/Tests/Entity/forForm/docFromParamTest.php
+++ AnalizPdvBundle/Tests/Entity/forForm/docFromParamTest.php
@@ -37,6 +37,7 @@ class docFromParamTest extends KernelTestCase
 			$test->setTypeDoc($value["typeDoc"]);
 				$test->setNumDoc($value["numDoc"]);
 					$test->setINN($value["INN"]);
+						$test->setDateCreateDoc($value["dateCreateDoc"]);
 		$violationList = $validator->validate($test);
 		$this->assertEquals($res, $violationList->count());
 		echo (string) $violationList."\n";
@@ -53,14 +54,16 @@ class docFromParamTest extends KernelTestCase
 		return [
 			// "numBranch"=>null,"numMainBranch"=>null в классе изменяются на "000" потому ошибки валидации нет
 			//"monthCreate"=>null,"yearCreate"=>null в классе изменяются на "0" потому валидацию не проходит
-			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"212//015","typeDoc"=>"ПНЕ","INN"=>"12132121кк"],1],
-			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"212//015","typeDoc"=>"ПНЕ","INN"=>"12132121"],0],
-			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"212//пп","typeDoc"=>"ПНЕ","INN"=>"12132121"],1],
-			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"212//0в","typeDoc"=>"ааа","INN"=>"12132121"],2],
-			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"212//015","typeDoc"=>"ПНЕ","INN"=>"12132121кк"],1],
-			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"212//015","typeDoc"=>"ПНЕ4","INN"=>"+12132121"],2],
-			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"","typeDoc"=>"","INN"=>"///"],3],
-			[["monthCreate"=>15,"yearCreate"=>2016,"numDoc"=>"","typeDoc"=>"","INN"=>""],4],
+			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"212//015","typeDoc"=>"ПНЕ","INN"=>"12132121кк","dateCreateDoc"=>null],1],
+			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"212//015","typeDoc"=>"ПНЕ","INN"=>"12132121","dateCreateDoc"=>null],0],
+			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"212//пп","typeDoc"=>"ПНЕ","INN"=>"12132121","dateCreateDoc"=>null],1],
+			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"212//0в","typeDoc"=>"ааа","INN"=>"12132121","dateCreateDoc"=>null],2],
+			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"212//015","typeDoc"=>"ПНЕ","INN"=>"12132121кк","dateCreateDoc"=>null],1],
+			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"212//015","typeDoc"=>"ПНЕ4","INN"=>"+12132121","dateCreateDoc"=>null],2],
+			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"","typeDoc"=>"","INN"=>"///","dateCreateDoc"=>null],2],
+			[["monthCreate"=>15,"yearCreate"=>2016,"numDoc"=>"","typeDoc"=>"","INN"=>"","dateCreateDoc"=>null],2],
+			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"","typeDoc"=>"ПНЕ","INN"=>"12132121","dateCreateDoc"=> new \DateTime("2016-02-01")],1],
+			[["monthCreate"=>1,"yearCreate"=>2016,"numDoc"=>"","typeDoc"=>"ПНЕ","INN"=>"12132121","dateCreateDoc"=> new \DateTime("2016-01-01")],0],
 
 
 		];
