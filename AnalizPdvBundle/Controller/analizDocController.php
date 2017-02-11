8c3953c44ec700694b7881708199dd9b528c1514
ivanov.nickolay <ivanov.nickolay@gmail.com>
2017-02-11 10:07:35 +0200

    Форма выбора периода и филиала. настройка выбора данных и отображения

--- AnalizPdvBundle/Controller/analizDocController.php
+++ AnalizPdvBundle/Controller/analizDocController.php
@@ -5,6 +5,7 @@ namespace AnalizPdvBundle\Controller;
 use AnalizPdvBundle\Entity\forForm\analiz\analizInnOut;
 use AnalizPdvBundle\Form\analizForm\analizInnOutForm;
 use AnalizPdvBundle\Form\analizForm\analizInnOutFormHandler;
+use AnalizPdvBundle\Model\getDataFromSQL\getDataOutINNByOne;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -33,13 +34,41 @@ class analizDocController extends Controller
 	    {
 	    	// если проверка прошла успешно то надо отобразить таблицу отклонений
 		    // writeAnalizOutByInn::writeAnalizPDVOutInnByOneBranchWithDoc
-
+			$criteriaAnaliz=$form->getData();
 
 
 	    }
 	    return $this->render('@AnalizPdv/form/analizForm.html.twig', array(
 		    'form' => $form->createView(),
 	    ));
+
+    }
+
+	/**
+	 * @param analizInnOut $analizInnOut
+	 */
+	private function getDataAnaliz(analizInnOut $analizInnOut)
+    {
+    	$Data=new getDataOutINNByOne($this->getDoctrine()->getManager());
+    	$typeAnaliz=$analizInnOut->getTypeAnaliz();
+
+    }
+
+	/**
+	 * @param string $type
+	 */
+	public function getMethodAnaliz(string $type)
+    {
+        $methodAnaliz=[
+        	["E=R"]=>["getReestrEqualErpn"],
+	        ["E<>R"]=>["getErpnNoEqualReestr"],
+	        ["R<>E"]=>["getReestrNoEqualErpn"],
+        ];
+	    if (array_key_exists($type, $methodAnaliz)) {
+		    return $methodAnaliz[$type];
+	    } else {
+	    	return null;
+	    }
 
     }
 }
